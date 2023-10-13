4efc60bed298755b34f38c5fd81c69371bbaf309
Kubilay Demir <87670246+kubilaydem@users.noreply.github.com>
2023-10-13 21:27:16 +0300

    5 round game done, project completed

---
+++ readme.md
@@ -0,0 +1,29 @@
+Basic Rock & Paper & Scissors Game
+
+-- Algorithm --
+
+get user choice with prompt box
+save user input to variable
+randomize computer choice
+save computer choice to variable
+compare computer and user choice
+create computer win conditions (if)
+create player win condition (else if)
+create tie condition (else)
+return lose / win / tie message with template literal
+repeat round 5 times
+display winner
+
+
+-- Possible conditions --
+
+-Player-    -Computer-   -Result-
+Rock        Rock         Tie            
+Paper       Rock         Player
+Scissors    Rock         Computer
+Rock        Paper        Computer
+Paper       Paper        Tie
+Scissors    Paper        Player
+Rock        Scissors     Player
+Paper       Scissors     Computer
+Scissors    Scissors     Tie
--- script.js
+++ script.js
@@ -1,54 +1,51 @@
-// get user choice with prompt box
-// save user input to variable
-// randomize computer choice
-// save computer choice to variable
-// compare computer and user choice
-// create computer win conditions (if)
-// create player win condition (else if)
-// create tie condition (else)
-// return lose / win / tie message with template literal
-// repeat round 5 times
-// display winner
-
-const items = ["rock", "paper", "scissor"];
+let computerScore = 0
+let playerScore = 0
+let currentRound = 1
 
 function getComputerChoice(){
+    const items = ["rock", "paper", "scissors"];
     return items[Math.floor(Math.random() * items.length)];
 }
 
-/* 
-Possible conditions
--Player- -Computer-  -Result-
-Rock      Rock       Tie            
-Paper     Rock       Player
-Scissor   Rock       Computer
-Rock      Paper      Computer
-Paper     Paper      Tie
-Scissor   Paper      Player
-Rock      Scissor    Player
-Paper     Scissor    Computer
-Scissor   Scissor    Tie
-*/
-
 function round(playerSelection, computerSelection) {
-    if (playerSelection === "scissor" && computerSelection === "rock" || 
+    if (playerSelection === "scissors" && computerSelection === "rock" || 
         playerSelection === "rock" && computerSelection === "paper" ||
-        playerSelection === "paper" && computerSelection === "scissor") {
+        playerSelection === "paper" && computerSelection === "scissors") {
             alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+            return (computerScore = ++computerScore, 
+                    currentRound = ++currentRound);
       } else if (playerSelection === "paper" && computerSelection === "rock" ||
-                 playerSelection === "scissor" && computerSelection ==="paper" ||
-                 playerSelection === "rock" && computerSelection === "scissor") {
+                 playerSelection === "scissors" && computerSelection ==="paper" ||
+                 playerSelection === "rock" && computerSelection === "scissors") {
                     alert(`You Win! ${playerSelection} beats ${computerSelection}`);
-                    return `You Win! ${playerSelection} beats ${computerSelection}`;
+                    return (playerScore = ++playerScore,
+                            currentRound = ++currentRound);
                  } else {
                         alert("Tie!");
-                        return "Tie!";
+                        return currentRound = ++currentRound;
                  }
 }
 
-let playerSelection = prompt("Rock, Paper or Scissor?").toLowerCase();
-console.log("player = " + playerSelection)
-const computerSelection = getComputerChoice()
-console.log("computer = " + computerSelection)
-round(playerSelection, computerSelection);
+function game() {
+    const playerSelection = prompt("Round:  " + currentRound + " | Choose your item |" + " Rock, Paper or Scissors?").toLowerCase();
+    const computerSelection = getComputerChoice()
+    round(playerSelection, computerSelection);
+    scoreCheck()
+    }
+
+function scoreCheck() {
+    alert("Player = " + playerScore + " & Computer = " + computerScore);
+    if (currentRound === 6 & computerScore > playerScore) {
+            alert("Computer wins this game.")
+        } else if (currentRound === 6 & computerScore < playerScore) {
+            alert("You win this game.")
+        } else if (currentRound === 6 & computerScore === playerScore) {
+            alert("It's draw, try again.")
+        }
+}
+
+game()
+game()
+game()
+game()
+game()
